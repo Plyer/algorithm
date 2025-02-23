f52cb94168c15c358657d2e6df1b8bdfaab207ed
LvJinglu <871586668@qq.com>
2025-02-23 16:48:11 +0800

    堆排序相关

--- src/main/java/cn/flyago/leetcode/lcsaf/binarysearch/MinNumberOfSeconds.java
+++ src/main/java/cn/flyago/leetcode/lcsaf/binarysearch/MinNumberOfSeconds.java
@@ -1,52 +1,30 @@
 package cn.flyago.leetcode.lcsaf.binarysearch;
 
-import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * 3296. 移山所需的最少秒数
  *
- * check 函数不对，使用堆排序更好解决。
- *
- * 二分查找：秒数越多，可移的山越高，符合单调性，可以使用二分。
- * 1. 秒数的范围，最小是 1，最大是高度为 10^5，仅有一个工人且时间是 10^6，最大秒数就是 10^6 + 10^6*2 + ... + 10^6*10^5。
- * 2. check 函数：a=mountainHeight/n b=mountainHeight%n，升序排序 workTimes，前 b 个工人需要降低 a+1 的高度，剩余工人需要降低 a 的高度，枚举 workTimes 把所有工时求和，如果和小于等于传入的秒数则符合要求。
+ * 最小堆：使用最小堆优先队列保存 n 个数组，每个数组的元素是 [nextWorkTime, delta, base]，nextWorkTime 表示下一次移动高度 1 的时间（包括之前花费的时间），delta 和 base 辅助计算 next。
  */
-//public class MinNumberOfSeconds {
-//    public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
-//        Arrays.sort(workerTimes);
-//        long maxHeight = time(1000000, 100000);
-//        long l = 1, r = maxHeight;
-//        while (l <= r) {
-//            long mid = l + (r - l) / 2;
-//            if (check(mountainHeight, workerTimes, mid)) {
-//                r = mid - 1;
-//            } else {
-//                l = mid + 1;
-//            }
-//        }
-//        return l > maxHeight ? -1 : l;
-//    }
-//
-//    private boolean check(int mountainHeight, int[] workerTimes, long m) {
-//        int n = workerTimes.length;
-//        int a = mountainHeight / n;
-//        int b = mountainHeight % n;
-//        long sum = 0;
-//        for (int i = 0; i < workerTimes.length; i++) {
-//            int height = a;
-//            if (i < b) {
-//                height++;
-//            }
-//            sum += time(workerTimes[i], height);
-//            if (sum > m) {
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-//
-//    private long time(int workerTime, int mountainHeight) {
-//        long c = (long) ((long) (1 + mountainHeight) * mountainHeight / (double) 2);
-//        return workerTime * c;
-//    }
-//}
+public class MinNumberOfSeconds {
+    public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
+        int n = workerTimes.length;
+        PriorityQueue<long[]> queue = new PriorityQueue<>(n, Comparator.comparing(arr -> arr[0]));
+        for (int t : workerTimes) {
+            queue.add(new long[]{t, t, t});
+        }
+
+        long res = 0;
+        while (mountainHeight-- > 0) {
+            long[] poll = queue.poll();
+            long next = poll[0], delta = poll[1], base = poll[2];
+            res = next;
+            poll[0] = next + delta + base;
+            poll[1] = delta + base;
+            queue.add(poll);
+        }
+        return res;
+    }
+}
